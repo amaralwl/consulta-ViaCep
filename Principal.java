3bfa215199ac0765c0d238ff6081239c165b2c88
amaralwl <163368895+amaralwl@users.noreply.github.com>
2024-04-30 01:51:05 -0300

    Projeto inicial

---
+++ Principal.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um CEP para consulta:");
+        var cep = sc.nextLine();
+
+        ConsultaCep consulta = new ConsultaCep();
+        try{
+            Endereco novoEndereco = consulta.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
+
+
+
+
+
+    }
+}
